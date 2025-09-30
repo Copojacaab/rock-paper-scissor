184127425011853dd0868265172dd778c3ce5610
Copojaab <palermijacopo@gmail.com>
2025-09-30 11:41:41 +0200

    i've connected the button with the variable and play round function

--- script.js
+++ script.js
@@ -1,7 +1,55 @@
+// UI
+function createUI(){
+    const divBtn = document.createElement('div');
+    
+
+    const btnRock = document.createElement('button');
+    const btnPaper = document.createElement('button');
+    const btnScissor = document.createElement('button');
+    btnRock.textContent = 'Pick Rock';
+    btnPaper.textContent = 'Pick Paper';
+    btnScissor.textContent = 'Pick Scissors';
+
+    btnRock.id = 'rock_button';
+    btnPaper.id = 'paper_button';
+    btnScissor.id = 'scissor_button';
+
+    divBtn.appendChild(btnRock);
+    divBtn.appendChild(btnPaper);
+    divBtn.appendChild(btnScissor);
+
+    document.body.appendChild(divBtn);
+
+    const divScore = document.createElement('div');
+    document.body.appendChild(divScore);
+    return {btnRock, btnPaper, btnScissor, divBtn, divScore};
+}
+
+const uiElements = createUI();
+
+function addEventListener(ui){
+    ui.btnRock.addEventListener('click', () => {
+        playRound(possibleChoice[0],ui);
+    });
+    
+    ui.btnPaper.addEventListener('click' ,() =>{
+        playRound(possibleChoice[1], ui);
+    });
+
+    ui.btnScissor.addEventListener('click', () => {
+        playRound(possibleChoice[2], ui);
+    });
+}
+
+// GAME
 const possibleChoice = ['rock','paper', 'scissor'];
 let computerScore = 0;
 let humanScore = 0;
 
+
+function updateScore(divScore, humanScore, computerScore){
+
+}
 
 // Logica per la scelta del computer
 function computerChoice(){
@@ -23,9 +71,8 @@ function humanChoice(choice){
     return choice;
 }
 
-function playRound(){
+function playRound(human, ui){
     let computer = computerChoice();
-    let human = humanChoice(prompt('Inserisci la tua scelta: '));
     console.log(computer + ' vs ' + human);
     // pareggio, restituisco null per indicare di rifare il round
     if (computer === human){
@@ -57,33 +104,10 @@ function playRound(){
         computerScore += 1;
         return;
     }
-
+    ui.divScore.textContent = "diocane";
 }
 
-// UI
-function createUI(){
-    const div = document.createElement('div');
-    
 
-    const btnRock = document.createElement('button');
-    const btnPaper = document.createElement('button');
-    const btnScissor = document.createElement('button');
-    btnRock.textContent = 'Pick Rock';
-    btnPaper.textContent = 'Pick Paper';
-    btnScissor.textContent = 'Pick Scissors';
 
-    btnRock.id = 'rock_button';
-    btnPaper.id = 'paper_button';
-    btnScissor.id = 'scissor_button';
+addEventListener(uiElements);
 
-    div.appendChild(btnRock);
-    div.appendChild(btnPaper);
-    div.appendChild(btnScissor);
-
-    document.body.appendChild(div);
-}
-
-function btnEventListener(){
-    btnRock.addEventListener()
-}
-createUI();
